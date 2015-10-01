0b3cc584501415e7a33e165cdb1a625d8a8f9f1a
zlodiak <prozaik81-2@yandex.ru>
2015-10-01 22:35:35 +0300

    before reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,11 @@ User.create!(:email => 'admin@ad.ad', :password => 'qwertyui', :password_confirm
 User.create!(:email => 'user1@ad.ad', :password => 'qwertyui', :password_confirmation => 'qwertyui')
 User.create!(:email => 'user2@ad.ad', :password => 'qwertyui', :password_confirmation => 'qwertyui')
 User.create!(:email => 'user3@ad.ad', :password => 'qwertyui', :password_confirmation => 'qwertyui')
+
+30.times do |n|
+  Email.create!(   
+    email: "subscriber#{n+1}@ad.ad",
+    description: Faker::Lorem.paragraph(7),
+    user_id: rand(1..4)
+  )
+end   
