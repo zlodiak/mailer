2afe7a44b2927b34f7b48feb727021b430072597
zlodiak <prozaik81-2@yandex.ru>
2015-10-01 13:23:54 +0300

    devise ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
   resources :emails
   # The priority is based upon order of creation: first created -> highest priority.
