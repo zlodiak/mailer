fe6550599ca33628e4042b3e9aaca01c9351036f
zlodiak <prozaik81-2@yandex.ru>
2015-10-01 11:13:02 +0300

    seeds admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+AdminUser.create!(:email => 'admin@ad.ad', :password => 'qwertyui', :password_confirmation => 'qwertyui')
