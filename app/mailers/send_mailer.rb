2467392e6da002d38686491528daa679941b40e5
zlodiak <prozaik81-2@yandex.ru>
2015-10-08 10:14:27 +0300

    fff

--- app/mailers/send_mailer.rb
+++ app/mailers/send_mailer.rb
@@ -4,7 +4,11 @@ class SendMailer < ApplicationMailer
   def sends_send(send, email)
     @send = send
     @email = email
-            
+    @image_name = send.image_file_name     
+    #@image_path = send.image.url(:medium)  
+    @image_path = Rails.root.join(send.image.url(:medium).split('?').first)    
+
+    #mail.attachments[@image_name] = File.read(@image_path)
     mail(to: email.email, subject: send.subject)
   end  
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  config.action_mailer.asset_host = "http://localhost:3000/public"
 end
