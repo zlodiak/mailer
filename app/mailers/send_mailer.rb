b61e4d0ba8ae4fd5e6fada92e0f3b879f0a2d52e
zlodiak <prozaik81-2@yandex.ru>
2015-10-08 10:26:35 +0300

    attach fail

--- app/mailers/send_mailer.rb
+++ app/mailers/send_mailer.rb
@@ -6,7 +6,7 @@ class SendMailer < ApplicationMailer
     @email = email
     @image_name = send.image_file_name     
     #@image_path = send.image.url(:medium)  
-    @image_path = Rails.root.join(send.image.url(:medium).split('?').first)    
+    @image_path = Rails.root.join('/public' + send.image.url(:medium).split('?').first.to_s)    
 
     #mail.attachments[@image_name] = File.read(@image_path)
     mail(to: email.email, subject: send.subject)
